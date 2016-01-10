5adba6d6309e693dc457ae9fe8cecbbe923f26cc
unknown <paxillum@mail.ru>
2016-01-11 00:37:39 +0200

    Fix smoke test.

--- model-impl/src/test/java/com/teamdev/chatimpl/test/SmokeTest.java
+++ model-impl/src/test/java/com/teamdev/chatimpl/test/SmokeTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class SmokeTest extends AbstractTest {
@@ -17,19 +18,14 @@ public class SmokeTest extends AbstractTest {
         final ChatRoomDTO testChat = chatRoomService.addChatRoom("test_chat");
         userManagementService.register(new RegisterUserDTO("login", "password", new Date()));
         final LoginDTO user = userAuthenticationService.login("login", "password");
-        final ChatRoomDTO lastChat = readLastChatRoom(new UserId(user.userId), new TokenDTO(user.token));
+        final Iterable<ChatRoomDTO> chatRoomDTOs = chatRoomService.readAllChatRooms(new UserId(user.userId), new TokenDTO(user.token));
 
-        assertEquals("Wrong chatroom id ", testChat.id, lastChat.id);
-        assertEquals("Wrong chatroom name ", testChat.name, lastChat.name);
-    }
-
-    private ChatRoomDTO readLastChatRoom(UserId userId, TokenDTO token) {
-        final Iterable<ChatRoomDTO> chatRoomDTOs = chatRoomService.readAllChatRooms(userId, token);
-        ChatRoomDTO lastChatRoom = null;
-        for (ChatRoomDTO chatRoomDTO : chatRoomDTOs) {
-            lastChatRoom = chatRoomDTO;
+        boolean chatInList = false;
+        for (ChatRoomDTO chatRoom : chatRoomDTOs) {
+            chatInList = chatInList || (chatRoom.id == testChat.id);
         }
-        return lastChatRoom;
+
+        assertTrue("test_chat was not found in chat list.", chatInList);
     }
 
 }
